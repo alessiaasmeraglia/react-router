cb63d8a9fc3ede77edb8f25610c2f2107191bcf8
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-12 15:24:44 +0200

    Definito rotte e aggiunto dipendenza corretta

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import { BroswerRouter, Routes, Route } from "react-router-dom";
 
 
 function App() {
-  
+  return (
+    <BroswerRouter>
+      
+      <Routes>
+        <Route path ="/" element = {<Home/>} />
+          <Route path ="/about" element = {<About/>} />
+          <Route path ="/products" element = {<Products/>} />
+      </Routes>
+    
+    </BroswerRouter>
+  );
 }
 
 export default App
