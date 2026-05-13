b62551de88d537b51f0aa1715b9358c03d2e2346
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-13 15:54:57 +0200

    Aggiunte costanti per useParams e useNavigate

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,9 @@ import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 
 function ProductDetail() {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    
     return (
         <h1>Pagina dettagli prodotti</h1>
     );
