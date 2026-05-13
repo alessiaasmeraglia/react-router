1e349be4f52a617ae5537ecf735439b0c41c8bfe
Alessia Smeraglia <alessia.smeraglia@gmail.com>
2026-05-13 14:46:23 +0200

    Esportato pagina ProductDetail

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Layout from "./components/Layout"
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/ProductDetail.jsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+function ProductDetail() {
+    return (
+        <h1>Pagina dettagli prodotti</h1>
+    );
+}
+
+export default ProductDetail;
